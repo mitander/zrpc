f774082f49ce4589308b71e52f48a5a5ef3e2c87
mitander <carlmitander@gmail.com>
2025-04-28 01:08:39 +0200

    build: simplify script

--- build.zig
+++ build.zig
@@ -4,70 +4,54 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // zrpc library
-    const lib_mod = b.createModule(.{
+    // Module
+    const lib = b.addModule("zrpc", .{
         .root_source_file = b.path("src/zrpc.zig"),
         .target = target,
         .optimize = optimize,
     });
-    lib_mod.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = "src" } });
 
-    const lib = b.addLibrary(.{
-        .linkage = .static,
-        .name = "zrpc",
-        .root_module = lib_mod,
-    });
-    b.installArtifact(lib);
-
-    // client executable
-    const client_exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/client_main.zig"),
+    // Library
+    const safe_lib = b.addStaticLibrary(.{
+        .name = "zrpc-safe",
+        .root_source_file = b.path("src/zrpc.zig"),
         .target = target,
-        .optimize = optimize,
+        .optimize = .ReleaseSafe,
     });
-    client_exe_mod.addImport("zrpc_lib", lib_mod);
-    client_exe_mod.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = "src" } });
+    const opts = b.addOptions();
+    opts.addOption(bool, "enable_assertions", true);
+    opts.addOption(bool, "enable_logging", false);
+    opts.addOption(bool, "enable_safety", true);
+    safe_lib.root_module.addOptions("build_options", opts);
+
+    // Client executable
     const client_exe = b.addExecutable(.{
-        .name = "zrpc-client",
-        .root_module = client_exe_mod,
-    });
-    b.installArtifact(client_exe);
-
-    const client_run_cmd = b.addRunArtifact(client_exe);
-    client_run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        client_run_cmd.addArgs(args);
-    }
-    const client_run_step = b.step("run-client", "Run the client");
-    client_run_step.dependOn(&client_run_cmd.step);
-
-    // server executable
-    const server_exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/server_main.zig"),
+        .name = "client",
+        .root_source_file = b.path("src/client.zig"),
         .target = target,
         .optimize = optimize,
     });
-    server_exe_mod.addImport("zrpc_lib", lib_mod);
-    server_exe_mod.addIncludePath(.{ .src_path = .{ .owner = b, .sub_path = "src" } });
+    client_exe.root_module.addImport("zrpc", lib);
+
+    // Server executable
     const server_exe = b.addExecutable(.{
-        .name = "zrpc-server",
-        .root_module = server_exe_mod,
+        .name = "server",
+        .root_source_file = b.path("src/server.zig"),
+        .target = target,
+        .optimize = optimize,
     });
-    b.installArtifact(server_exe);
+    server_exe.root_module.addImport("zrpc", lib);
 
-    const server_run_cmd = b.addRunArtifact(server_exe);
-    server_run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        server_run_cmd.addArgs(args);
-    }
-    const server_run_step = b.step("run-server", "Run the server");
-    server_run_step.dependOn(&server_run_cmd.step);
-
-    // tests
-    const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
+    // Testing
+    const tests = b.addTest(.{
+        .root_source_file = b.path("src/zrpc.zig"),
+        .target = target,
+        .optimize = optimize,
     });
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
+
+    // Steps
+    b.step("client", "Run client").dependOn(&b.addRunArtifact(client_exe).step);
+    b.step("server", "Run server").dependOn(&b.addRunArtifact(server_exe).step);
+    b.step("test", "Run tests").dependOn(&b.addRunArtifact(tests).step);
+    b.step("check", "ReleaseSafe validation").dependOn(&safe_lib.step);
 }
